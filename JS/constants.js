04d7b0f64f307282f1b3a294819fd12d5e5688f8
Deepak Patidar <153281433+devvang8094@users.noreply.github.com>
2025-12-13 16:03:02 +0530

    Update BASE_URL to new production endpoint

--- JS/constants.js
+++ JS/constants.js
@@ -1,5 +1,5 @@
-const BASE_URL = "https://library-backend-production-38a7.up.railway.app";
+// const BASE_URL = "https://library-backend-production-38a7.up.railway.app";
 
+const BASE_URL = "https://library-backend-kcxd.onrender.com"
 
-
-// const BASE_URL = "http://localhost:5500";
+// // const BASE_URL = "http://localhost:5500";
